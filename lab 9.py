928bf158df888d0acef8a5d19c3dc30025b8ddd2
demetriomarinez <demarinez2005@gmail>
2024-07-24 14:23:01 -0400

    added menu and encode function

--- lab 9.py
+++ lab 9.py
@@ -1,2 +1,27 @@
-print("Hello")
+def encode(password):
+    password_list = []
+    for char in password:
+        new_num = int(char) +3
+        password_list.append(str(new_num))
+    return ''.join(password_list)
 
+
+
+
+while True:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    option = int(input("Please enter an option: "))
+    if option == 1:
+        password = input("Please enter the password to encode: ")
+        encode(password)
+        print("Your password has been encoded and stored!")
+
+    elif option == 2:
+
+    elif option ==3:
+        print("")
+        break
